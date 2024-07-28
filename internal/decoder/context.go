c861f08cee5943c90d14e09469303ef8af0df67b
Trim21 <trim21.me@gmail.com>
2024-07-28 20:03:41 +0800

    refactor: avoid unnecessary buffer ref

--- internal/decoder/context.go
+++ internal/decoder/context.go
@@ -8,18 +8,17 @@ type Context struct {
 	Buf []byte
 }
 
-var (
-	ctxPool = sync.Pool{
-		New: func() any {
-			return &Context{}
-		},
-	}
-)
+var ctxPool = sync.Pool{
+	New: func() any {
+		return &Context{}
+	},
+}
 
 func newCtx() *Context {
 	return ctxPool.Get().(*Context)
 }
 
 func freeCtx(ctx *Context) {
+	ctx.Buf = nil
 	ctxPool.Put(ctx)
 }
