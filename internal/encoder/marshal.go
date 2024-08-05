4b33a3a7f1d3d8eed7df2a0978776b05ccf3df7c
Trim21 <trim21.me@gmail.com>
2024-08-05 18:58:11 +0800

    refactor: remove unnecessary method

--- internal/encoder/marshal.go
+++ internal/encoder/marshal.go
@@ -5,23 +5,14 @@ import (
 )
 
 func MarshalCtx(ctx *Context, v any) error {
-	b, err := encode(ctx, ctx.Buf, v)
-	if err != nil {
-		return err
-	}
-
-	ctx.Buf = b
-
-	return nil
-}
-
-func encode(ctx *Context, b []byte, v any) ([]byte, error) {
 	rv := reflect.ValueOf(v)
 
 	enc, err := compileWithCache(rv.Type())
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return enc(ctx, b, rv)
+	ctx.Buf, err = enc(ctx, ctx.Buf, rv)
+
+	return err
 }
